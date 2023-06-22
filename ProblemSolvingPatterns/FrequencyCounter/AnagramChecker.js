f91223bf7ebd238c63d3496e8042e5abf8d1dde4
Ian Miller <indmillr@gmail.com>
2023-06-22 09:43:56 -0500

    Frequency Counter Pattern
    1. A function to compare strings to determine if Anagram
    2. A function to compare arrays to determine if second array contains squares of first array

---
+++ ProblemSolvingPatterns/FrequencyCounter/AnagramChecker.js
@@ -0,0 +1,27 @@
+// given two strings, determine if the second string is an anagram of the first
+
+function validAnagram(first, second) {
+    if (first.length !== second.length) {
+        return false;
+    }
+
+    const lookup = {};
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i];
+        // if letter exists, increment; otherwise set to 1
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i];
+        // can't find letter or letter is zero, then not Anagram
+        if (!lookup[letter]) {
+            return false;
+        } else {
+            lookup[letter] -= 1;
+        }
+    }
+
+    return true;
+}
